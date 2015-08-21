acca12016c1310e3f97c6af8130038719c39db5d
Yen-chi Chen <mark86092@gmail.com>
2015-08-22 01:14:29 +0800

    update the crawler

--- cwb_radar/radar.php
+++ cwb_radar/radar.php
@@ -1,8 +1,8 @@
 <?php
 require_once('curl.php');
 
-function get_radar_at_all() {
-	$r = query('http://www.cwb.gov.tw/V7/js/MOS2_1024.js');
+function get_radar_list($url) {
+	$r = query($url);
 	$a = substr($r['body'], 16);
 
 	preg_match_all('/\"(.*)\":\"(.*)\"/', $a, $matches, PREG_SET_ORDER);
@@ -14,6 +14,18 @@ function get_radar_at_all() {
 		);
 	}
 	return $data;
+}
+
+function get_radar_at_N() {
+	return get_radar_list('http://www.cwb.gov.tw/V7/js/MOS2_1024N.js');
+}
+
+function get_radar_at_S() {
+	return get_radar_list('http://www.cwb.gov.tw/V7/js/MOS2_1024S.js');
+}
+
+function get_radar_at_all() {
+	return get_radar_list('http://www.cwb.gov.tw/V7/js/MOS2_1024.js');
 }
 
 function get_radar_image($url, $filename) {
---
+++ cwb_radar/s.php
@@ -0,0 +1,28 @@
+<?php
+require_once('conf.php');
+require_once('CWB_Radar.php');
+require_once('radar.php');
+
+$mysqli = new mysqli("localhost", mysql_user, mysql_pass, 'cwb_data');
+
+if (mysqli_connect_errno()) {
+	printf("Connect failed: %s\n", mysqli_connect_error());
+	exit();
+}
+$mysqli->set_charset("utf8");
+
+$data = get_radar_at_S();
+
+foreach($data as $m) {
+	$c = CWB_Radar::get($mysqli, 's', $m['time']);
+	if (is_null($c)) {
+		$filename = basename($m['url']);
+		echo $filename . ' should download' . "\n";
+		
+		get_radar_image($m['url'], 'img/' . $filename);
+
+		CWB_Radar::insert($mysqli, 's', $m['time'], $filename);
+	}
+}
+
+$mysqli->close();
