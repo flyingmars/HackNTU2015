0b7c9af6714720aab5f33c8d851480ba3f840f5a
Yen-chi Chen <mark86092@gmail.com>
2015-08-22 12:58:49 +0800

    add CORS

--- forecast.php
+++ forecast.php
@@ -1,6 +1,10 @@
 <?php
 
-    // probability in [0,1]
-    // Intensity in mm/day
-    $output = array('probability' => 0.9 , 'intensity' => 3.0 );
-    echo json_encode($output) ;
+// probability in [0,1]
+// Intensity in mm/day
+$output = array('probability' => 0.9 , 'intensity' => 3.0 );
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: *");
+header("Content-Type: application/json");
+echo json_encode($output) ;
