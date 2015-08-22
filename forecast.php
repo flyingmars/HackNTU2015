c4fd87931202347f4f0fdddfcf942d7a8f6b4242
Kevin Chen <flyingmars@gmail.com>
2015-08-22 12:46:27 +0800

    fix bug of output

--- forecast.php
+++ forecast.php
@@ -3,4 +3,4 @@
     // probability in [0,1]
     // Intensity in mm/day
     $output = array('probability' => 0.9 , 'intensity' => 3.0 );
-    echo json_encode($arr) ;
+    echo json_encode($output) ;
