1dd6ea6f3ff5083a3510af93255be7a5df85428e
Ｍark_mac <mark_mac@ark-macdeMacBook-Pro.local>
2015-08-22 14:58:59 +0800

    add getInfo.js

---
+++ view/js/getInfo.js
@@ -0,0 +1,13 @@
+//to get info from http://temp1.mar98.tk/HackNTU2015/forecast.php
+// input are probability and intensity
+$.getJSON("http://temp1.mar98.tk/HackNTU2015/forecast.php", function(data) {
+    var items = [];
+    $.each(data, function(key, val) {
+        items.push("<p> The " + key + " of rain is " + val + ".</p>");
+    });
+
+    $("<ul/>", {
+        "class": "my-new-list",
+        html: items.join("")
+    }).appendTo("#show");
+});
