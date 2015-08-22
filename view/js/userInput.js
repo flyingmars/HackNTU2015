8f6b6f02cd6a376a488c81f1e03532b03e494220
Ｍark_mac <mark_mac@ark-macdeMacBook-Pro.local>
2015-08-23 01:43:07 +0800

    fix input problem

--- view/js/userInput.js
+++ view/js/userInput.js
@@ -2,7 +2,13 @@ var input = [];
 $("#submit").click(function() {
     input[0] = $('#longitude').val();
     input[1] = $('#latitude').val();
-    input[2] = $('#date').val() + " " + $('#time').val();
+    input[2] = $('select[name="year"]').find(":selected").text();
+    input[3] = $('select[name="month"]').find(":selected").text();
+    input[4] = $('select[name="date"]').find(":selected").text();
+    input[5] = $('select[name="hour"]').find(":selected").text();
+    input[6] = $('select[name="minute"]').find(":selected").text();
+
+    var $time = input[2]+"-"+input[3]+"-"+input[4]+" "+input[5]+":"+input[6]+":00";
 
     //show all elem for input
     /* 
@@ -20,7 +26,7 @@ $("#submit").click(function() {
     var elem = {
         longitude: input[0],
         latitude: input[1],
-        time: input[2]
+        time: $time
     };
 
     $.getJSON("http://temp2.mar98.tk/HackNTU2015/forecast.php", elem, function(data) {
