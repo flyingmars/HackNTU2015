655e5ded4c6b70d5d1c3f9febed1f20dd90f3b03
Yen-chi Chen <mark86092@gmail.com>
2015-08-22 14:43:36 +0800

    fake forecast done

--- forecast.php
+++ forecast.php
@@ -1,10 +1,27 @@
 <?php
+$latitude = $_GET['latitude'];
+$longitude = $_GET['longitude'];
+$time = $_GET['time'];
+
+if (is_null($latitude) || is_null($longitude) || is_null($time)) {
+	header("Access-Control-Allow-Origin: *");
+	header("Access-Control-Allow-Methods: *");
+	header("Content-Type: application/json");
+	echo json_encode(array('status' => 'error'));
+	exit;
+}
 
 // probability in [0,1]
 // Intensity in mm/day
-$output = array('probability' => 0.9 , 'intensity' => 3.0 );
+$output = array(
+	'latitude' => $latitude, 
+	'longitude' => $longitude,
+	'time' => $time,
+	'probability' => 0.9,
+	'intensity' => 3.0
+);
 
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: *");
 header("Content-Type: application/json");
-echo json_encode($output) ;
+echo json_encode(array('status' => 'success', 'result' => $output));
