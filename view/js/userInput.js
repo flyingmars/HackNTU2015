db1299c99c39ec3e8b335b60b4e818f92cc245db
Yen-chi Chen <r02922044@ntu.edu.tw>
2015-08-23 09:31:48 +0800

    Update userInput.js

--- view/js/userInput.js
+++ view/js/userInput.js
@@ -17,6 +17,8 @@ $("#submit").click(function() {
     };
 
     $.getJSON("http://temp2.mar98.tk/HackNTU2015/forecast.php", elem, function(data) {
+    	// logging the results
+    	console.log(data);
 
         if (data["status"] == "success") {
             var items = [];
